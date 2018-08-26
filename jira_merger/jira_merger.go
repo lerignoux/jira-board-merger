329cfde1803f450e375cc619a3d06bfc27109858
Laurent Erignoux <lerignoux@gmail.com>
2018-08-26 23:54:06 +0800

    Validating golang syntax and code and adding draft of sub requests processing

--- jira_merger/jira_merger.go
+++ jira_merger/jira_merger.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "bytes"
     "fmt"
     "html"
     "log"
@@ -12,38 +13,65 @@ import (
 )
 
 type Configuration struct {
-    servers,
-    aggregated_keys [string]
+    servers         []ServerConf `json:"array"`
+    aggregated_keys []string
 }
 
-file, _ := os.Open("config.json")
-defer file.Close()
-decoder := json.NewDecoder(file)
-configuration := Configuration{}
-err := decoder.Decode(&configuration)
-if err != nil {
-  fmt.Println("error:", err)
+type ServerConf struct {
+    host     string  `json:"host"`
 }
+
+var configuration Configuration
+var httpClient = &http.Client{}
 
 func main() {
-
+		loadConfig("config.json")
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/allData", GetAllData)
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+func loadConfig(configName string) {
+	file, _ := os.Open(configName)
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+	  fmt.Println("error:", err)
+	}
+}
+
 func GetAllData(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    data := [len(configuration.servers)]
-    for i, server := configuration.servers {
-      data[i] = dataFetchServerData(server, r)
+    data := make([]string, len(configuration.servers))
+    for i, server := range configuration.servers {
+      data[i] = FetchServerData(server, r)
     }
-    merged = MergeData(data)
-    json.NewEncoder(w).Encode(todos)
+    merged := MergeData(data...)
+    json.NewEncoder(w).Encode(merged)
 }
 
-func FetchServerData(server string, initialRequest *http.Request) {
+func FetchServerData(server ServerConf, initialRequest *http.Request) string {
+	initialRequest.URL.Query().Set("hostname", server.host)
+	req, err := http.NewRequest("GET", initialRequest.URL.String(), nil)
+	if err != nil {
+      fmt.Printf("Error : %s", err)
+  }
+	resp, err := httpClient.Do(req)
+	if err != nil {
+      fmt.Printf("Error : %s", err)
+  }
+  fmt.Println("resp")
+
+  var b bytes.Buffer
+  _, err = b.ReadFrom(resp.Body)
+  if err != nil {
+      log.Fatal("Error : %s", err)
+  }
+	return b.String()
 }
 
-func MergeData(data) {
+func MergeData(data ...string) string {
+	return "ok"
 }
